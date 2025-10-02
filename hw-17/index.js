75b151a3ac3a1256fe26997968a4b39fd7b4990a
Artur-Vishnevskiy <arturvisnevskij01@gmail.com>
2025-10-02 20:51:36 +0300

    task 1

---
+++ hw-17/index.js
@@ -0,0 +1,13 @@
+// Задание 1
+
+let word1 = "true";
+let word2 = false;
+let word3 = 17;
+let word4 = undefined;
+let word5 = null;
+
+console.log(typeof(word1));
+console.log(typeof(word2));
+console.log(typeof(word3));
+console.log(typeof(word4));
+console.log(typeof(word5));
