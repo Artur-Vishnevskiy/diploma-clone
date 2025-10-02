4d6e8b9152874bf41dbad461f6ebacd53eb14a86
Artur-Vishnevskiy <arturvisnevskij01@gmail.com>
2025-10-02 15:54:39 +0300

    task 2 and 3

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ let result = x + y + z;
 console.log(result);
 
 // Задание 2
+
+let secMin = 60;
+let minHour = 60;
+let hourDay = 24;
+let dayYear = 365;
+let myAge = 19;
+myAgeSeconds = myAge * dayYear * hourDay * minHour * secMin
+console.log(myAgeSeconds);
+
+//Задание 3
+
+let count = 42;
+let userName = '42';
+
+console.log(String(count));
+console.log(Number(userName));
+console.log(`${count}`);
+console.log(+userName);
