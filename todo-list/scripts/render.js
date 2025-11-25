f29c1e57729209595c55bac0b13d859f8a3648eb
Artur-Vishnevskiy <arturvisnevskij01@gmail.com>
2025-11-25 20:34:31 +0300

    ready

--- todo-list/scripts/render.js
+++
@@ -1,7 +0,0 @@
-import { CARD_ELEMENT } from "./components/Card.js";
-
-export function renderTasks(tasks) {
-  const tasksElements = tasks.map((task) => CARD_ELEMENT(task));
-
-  return tasksElements;
-}
--- todo-list/scripts/state/constants.js
+++
@@ -1,2 +0,0 @@
-export const STORAGE_KEY = "todo-list";
-export const DEFAULTE_TASKS = [];
